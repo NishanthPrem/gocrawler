293136ee7956fe152185084df5ff8b43f886a13b
Nishanth Prem <nishanthprem8@gmail.com>
2024-08-29 17:43:00 -0600

    Crawling the webpage and added error handling.

--- main.go
+++ main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 )
 
 func main() {
-	parseArgs()
+	link := parseArgs()
+	fmt.Println(getHTML(link))
 
 }
 
-func parseArgs() {
+func parseArgs() string {
 	args := os.Args[1:]
 	if len(args) < 1 {
 		fmt.Println("no website provided")
@@ -18,7 +21,32 @@ func parseArgs() {
 	} else if len(args) > 1 {
 		fmt.Println("too many arguments provided")
 		os.Exit(1)
-	} else {
-		fmt.Println("starting crawl of:", args[0])
 	}
+	fmt.Println("starting crawl of:", args[0])
+	return args[0]
+}
+
+func getHTML(rawURL string) (string, error) {
+	req, err := http.Get(rawURL)
+	if err != nil {
+		return "", fmt.Errorf("error fetching the data %w", err)
+	}
+
+	defer req.Body.Close()
+
+	if req.StatusCode > 399 {
+		return "", fmt.Errorf("request failed with status %d: %s", req.StatusCode, http.StatusText(req.StatusCode))
+	}
+
+	contentType := req.Header.Get("Content-Type")
+	if contentType != "text/html" {
+		return "", fmt.Errorf("unexpected content type: %s", contentType)
+	}
+
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		return "", fmt.Errorf("error reading body %w", err)
+	}
+
+	return string(body), nil
 }
